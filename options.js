59b04c85138493d6d542d5b4f4e950322ec1efd8
Malte Wilhelm <wmalte@vmware.com>
2023-02-09 18:11:18 +0100

    Make options page more slick

--- options.js
+++ options.js
@@ -18,16 +18,13 @@ document.addEventListener("DOMContentLoaded", function () {
     chrome.storage.sync.set({ time: defaultTime });
 
     // Show the defaults
-    let originalTime, originalCode;
     chrome.storage.sync.get(["time", "code"], function (data) {
-        originalTime = data.time;
-        originalCode = data.code;
+        let originalTime = data.time;
+        let originalCode = data.code;
+        timer.value = data.time;
 
-        document.getElementById("time").value = data.time;
-
-        let code = data.code;
-        if (code) {
-            document.getElementById("textarea").value = code;
+        if (originalCode) {
+            document.getElementById("textarea").value = originalCode;
             updateLineNumbers();
         }
     });
@@ -64,15 +61,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let saveBtn = document.getElementById("save");
     let saveCodeBtn = document.getElementById("save-code");
+    let resetBtn = document.getElementById("reset");
+    let timer = document.getElementById("time")
+
     saveBtn.setAttribute("disabled", "true");
     saveCodeBtn.setAttribute("disabled", "true");
-    document.getElementById("time").addEventListener("input", function () {
+    resetBtn.setAttribute("disabled", "true");
+
+    timer.addEventListener("input", function () {
         if (originalTime !== this.value) {
             saveBtn.removeAttribute("disabled");
         } else {
             saveBtn.setAttribute("disabled", "true");
         }
     });
+
     textarea.addEventListener("input", function () {
         if (originalCode !== this.value) {
             saveCodeBtn.removeAttribute("disabled");
@@ -81,33 +84,40 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    function toggleFeedbackBlock(feedback) {
+        feedback.style.display = 'block';
+        setTimeout(function () {
+            feedback.style.display = 'none';
+        }, 5000);
+    }
 
     // Save time value on save button click
-    document.getElementById("save").addEventListener("click", function () {
-        let time = document.getElementById("time").value;
+    saveBtn.addEventListener("click", function () {
+        let time = timer.value;
         chrome.storage.sync.set({ time: time }, function () {
             // Show a feedback to indicate successful save
-            let feedback = "Time saved successfully!";
-            document.getElementById("feedback").innerHTML = feedback;
+            saveBtn.setAttribute("disabled", "true");
+            resetBtn.removeAttribute("disabled");
+            toggleFeedbackBlock(document.getElementById('feedback-success'));
         });
     });
 
     // Save code value on save code button click
-    document.getElementById("save-code").addEventListener("click", function () {
+    saveCodeBtn.addEventListener("click", function () {
         let code = document.getElementById("textarea").value;
         chrome.storage.sync.set({ code: code }, function () {
-            document.getElementById("feedback").innerHTML = "Code saved.";
+            toggleFeedbackBlock(document.getElementById('feedback-success'));
         });
     });
 
     // Reset time value to default on reset button click
-    document.getElementById("reset").addEventListener("click", function () {
+    resetBtn.addEventListener("click", function () {
         chrome.storage.sync.set({ time: defaultTime }, function () {
             // Show a feedback to indicate successful reset
-            let feedback = "Time reset to default successfully!";
-            document.getElementById("feedback").innerHTML = feedback;
             // Update the value in the text field
-            document.getElementById("time").value = defaultTime;
+            timer.value = defaultTime;
+            resetBtn.setAttribute("disabled", "true");
+            toggleFeedbackBlock(document.getElementById('feedback-reset'));
         });
     });
 });
