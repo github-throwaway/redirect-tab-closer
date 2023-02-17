f8333b20d5da8dd7e94febd643682eb6d44f1252
Malte Wilhelm <wmalte@vmware.com>
2023-02-17 12:43:40 +0100

    Enforce ESLint

--- options.js
+++ options.js
@@ -1,124 +1,124 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOM Content loaded");
-    let originalTime,originalCode;
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOM Content loaded');
+  let originalTime; let originalCode;
 
-    let defaultUrls = [
-        "*://*zoom.us/postattendee",
-        "*://*.zoom.us/j/*",
-        "*://*zoom.us/s/*",
-        "*://*.zoom.us/s/*",
-        "*://*.slack.com/archives/*",
-        "*://*.webex.com/webappng/sites/*/meeting/info/*",
-        "*://*.webex.com/wbxmjs/joinservice/*",
-        "*://*teams.microsoft.com/dl/launcher/launcher.html*"
-    ];
-    let defaultTime = 10;
+  const defaultUrls = [
+    '*://*zoom.us/postattendee',
+    '*://*.zoom.us/j/*',
+    '*://*zoom.us/s/*',
+    '*://*.zoom.us/s/*',
+    '*://*.slack.com/archives/*',
+    '*://*.webex.com/webappng/sites/*/meeting/info/*',
+    '*://*.webex.com/wbxmjs/joinservice/*',
+    '*://*teams.microsoft.com/dl/launcher/launcher.html*',
+  ];
+  const defaultTime = 10;
 
-    // Set defaults
-    chrome.storage.sync.set({ code: defaultUrls.join("\n") });
-    chrome.storage.sync.set({ time: defaultTime });
+  // Set defaults
+  chrome.storage.sync.set({code: defaultUrls.join('\n')});
+  chrome.storage.sync.set({time: defaultTime});
 
-    // Show the defaults
-    chrome.storage.sync.get(["time", "code"], function (data) {
-        originalTime = data.time;
-        originalCode = data.code;
-        timer.value = data.time;
+  // Show the defaults
+  chrome.storage.sync.get(['time', 'code'], function(data) {
+    originalTime = data.time;
+    originalCode = data.code;
+    timer.value = data.time;
 
-        if (originalCode) {
-            document.getElementById("textarea").value = originalCode;
-            updateLineNumbers();
-        }
-    });
-
-    function updateLineNumbers() {
-        let lineNumbers = document.querySelector(".line-numbers");
-        let numberOfLines = textarea.value.split("\n").length;
-        lineNumbers.innerHTML = Array(numberOfLines)
-            .fill("<span></span>")
-            .join("");
+    if (originalCode) {
+      document.getElementById('textarea').value = originalCode;
+      updateLineNumbers();
     }
+  });
+
+  function updateLineNumbers() {
+    const lineNumbers = document.querySelector('.line-numbers');
+    const numberOfLines = textarea.value.split('\n').length;
+    lineNumbers.innerHTML = Array(numberOfLines)
+        .fill('<span></span>')
+        .join('');
+  }
 
 
-    console.log("Attach event listeners")
-    let textarea = document.querySelector("#textarea");
-    textarea.addEventListener("keyup", (event) => {
-        updateLineNumbers();
-    });
+  console.log('Attach event listeners');
+  const textarea = document.querySelector('#textarea');
+  textarea.addEventListener('keyup', (event) => {
+    updateLineNumbers();
+  });
 
 
-    textarea.addEventListener("keydown", (event) => {
-        if (event.key === "Tab") {
-            const start = textarea.selectionStart;
-            const end = textarea.selectionEnd;
+  textarea.addEventListener('keydown', (event) => {
+    if (event.key === 'Tab') {
+      const start = textarea.selectionStart;
+      const end = textarea.selectionEnd;
 
-            textarea.value =
+      textarea.value =
                 textarea.value.substring(0, start) +
-                "\t" +
+                '\t' +
                 textarea.value.substring(end);
 
-            event.preventDefault();
-        }
-    });
-
-    let saveBtn = document.getElementById("save");
-    let saveCodeBtn = document.getElementById("save-code");
-    let resetBtn = document.getElementById("reset");
-    let timer = document.getElementById("time")
-
-    saveBtn.setAttribute("disabled", "true");
-    saveCodeBtn.setAttribute("disabled", "true");
-    resetBtn.setAttribute("disabled", "true");
-
-    timer.addEventListener("input", function () {
-        if (originalTime !== this.value) {
-            saveBtn.removeAttribute("disabled");
-        } else {
-            saveBtn.setAttribute("disabled", "true");
-        }
-    });
-
-    textarea.addEventListener("input", function () {
-        if (originalCode !== this.value) {
-            saveCodeBtn.removeAttribute("disabled");
-        } else {
-            saveCodeBtn.setAttribute("disabled", "true");
-        }
-    });
-
-    function toggleFeedbackBlock(feedback) {
-        feedback.style.display = 'block';
-        setTimeout(function () {
-            feedback.style.display = 'none';
-        }, 5000);
+      event.preventDefault();
     }
+  });
 
-    // Save time value on save button click
-    saveBtn.addEventListener("click", function () {
-        let time = timer.value;
-        chrome.storage.sync.set({ time: time }, function () {
-            // Show a feedback to indicate successful save
-            saveBtn.setAttribute("disabled", "true");
-            resetBtn.removeAttribute("disabled");
-            toggleFeedbackBlock(document.getElementById('feedback-success'));
-        });
-    });
+  const saveBtn = document.getElementById('save');
+  const saveCodeBtn = document.getElementById('save-code');
+  const resetBtn = document.getElementById('reset');
+  const timer = document.getElementById('time');
 
-    // Save code value on save code button click
-    saveCodeBtn.addEventListener("click", function () {
-        let code = document.getElementById("textarea").value;
-        chrome.storage.sync.set({ code: code }, function () {
-            toggleFeedbackBlock(document.getElementById('feedback-success'));
-        });
-    });
+  saveBtn.setAttribute('disabled', 'true');
+  saveCodeBtn.setAttribute('disabled', 'true');
+  resetBtn.setAttribute('disabled', 'true');
 
-    // Reset time value to default on reset button click
-    resetBtn.addEventListener("click", function () {
-        chrome.storage.sync.set({ time: defaultTime }, function () {
-            // Show a feedback to indicate successful reset
-            // Update the value in the text field
-            timer.value = defaultTime;
-            resetBtn.setAttribute("disabled", "true");
-            toggleFeedbackBlock(document.getElementById('feedback-reset'));
-        });
+  timer.addEventListener('input', function() {
+    if (originalTime !== this.value) {
+      saveBtn.removeAttribute('disabled');
+    } else {
+      saveBtn.setAttribute('disabled', 'true');
+    }
+  });
+
+  textarea.addEventListener('input', function() {
+    if (originalCode !== this.value) {
+      saveCodeBtn.removeAttribute('disabled');
+    } else {
+      saveCodeBtn.setAttribute('disabled', 'true');
+    }
+  });
+
+  function toggleFeedbackBlock(feedback) {
+    feedback.style.display = 'block';
+    setTimeout(function() {
+      feedback.style.display = 'none';
+    }, 5000);
+  }
+
+  // Save time value on save button click
+  saveBtn.addEventListener('click', function() {
+    const time = timer.value;
+    chrome.storage.sync.set({time: time}, function() {
+      // Show a feedback to indicate successful save
+      saveBtn.setAttribute('disabled', 'true');
+      resetBtn.removeAttribute('disabled');
+      toggleFeedbackBlock(document.getElementById('feedback-success'));
     });
+  });
+
+  // Save code value on save code button click
+  saveCodeBtn.addEventListener('click', function() {
+    const code = document.getElementById('textarea').value;
+    chrome.storage.sync.set({code: code}, function() {
+      toggleFeedbackBlock(document.getElementById('feedback-success'));
+    });
+  });
+
+  // Reset time value to default on reset button click
+  resetBtn.addEventListener('click', function() {
+    chrome.storage.sync.set({time: defaultTime}, function() {
+      // Show a feedback to indicate successful reset
+      // Update the value in the text field
+      timer.value = defaultTime;
+      resetBtn.setAttribute('disabled', 'true');
+      toggleFeedbackBlock(document.getElementById('feedback-reset'));
+    });
+  });
 });
